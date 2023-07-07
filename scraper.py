2b0254645095d858c2fa4a218f1e14a04111aee4
AMEER HAMZA <114941881+AMEER-HAMZA-MAU@users.noreply.github.com>
2023-07-07 21:28:10 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-Python
+from Python
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -12,27 +12,27 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['mcs_assignment']
 collection = db['quotes']
 for page in range(10):
-for i in range(1, 11):
-quote_xpath = f"/html/body/div[1]/div[2]/div[1]/div[{i}]/span[1]"
-author_xpath =
+  for i in range(1, 11):
+      quote_xpath = f"/html/body/div[1]/div[2]/div[1]/div[{i}]/span[1]"
+      author_xpath =
 f"/html/body/div[1]/div[2]/div[1]/div[{i}]/span[2]/small"
-quote_element = driver.find_element(By.XPATH, quote_xpath)
-quote_text = quote_element.text
-author_element = driver.find_element(By.XPATH, author_xpath)
-author_name = author_element.text
-print("Quote:", quote_text)
-print("Author:", author_name)
-print()
-document = {
-'quote_id': page * 10 + i,
-'quote': quote_text,
-'author': author_name
-}
-collection.insert_one(document)
+      quote_element = driver.find_element(By.XPATH, quote_xpath)
+      quote_text = quote_element.text
+      author_element = driver.find_element(By.XPATH, author_xpath)
+      author_name = author_element.text
+      print("Quote:", quote_text)
+      print("Author:", author_name)
+      print()
+      document = {
+        'quote_id': page * 10 + i,
+        'quote': quote_text,
+        'author': author_name
+      }
+      collection.insert_one(document)
 try:
-next_button = driver.find_element(By.XPATH, "//li[@class='next']/a")
-next_button.click()
+    next_button = driver.find_element(By.XPATH, "//li[@class='next']/a")
+    next_button.click()
 except:
-print("No more pages available. Exiting loop.")
-break
+    print("No more pages available. Exiting loop.")
+    break
 driver.quit()
